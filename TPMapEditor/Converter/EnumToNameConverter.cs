b11d48876e07c2213b2f189515745f261dc6eeab
Randhomme <randormi@gmail.com>
2025-11-25 18:52:13 +0100

    feat: use GetShortName instead of GetName

--- TPMapEditor/Converter/EnumToNameConverter.cs
+++ TPMapEditor/Converter/EnumToNameConverter.cs
@@ -10,7 +10,7 @@ namespace TPMapEditor.Converter
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is Enum enumValue)
-                return EnumExtensions.GetName(enumValue);
+                return EnumExtensions.GetShortName(enumValue);
 
             return string.Empty;
         }
--- TPMapEditor/Enums/EnumExtensions.cs
+++ TPMapEditor/Enums/EnumExtensions.cs
@@ -13,6 +13,13 @@ namespace TPMapEditor.Enums
             return attribute?.Name ?? value.ToString();
         }
 
+        public static string GetShortName(this Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            var attribute = (DisplayAttribute)Attribute.GetCustomAttribute(field, typeof(DisplayAttribute));
+            return attribute?.ShortName ?? attribute?.Name ?? value.ToString();
+        }
+
         public static string GetDescription(this Enum value)
         {
             var field = value.GetType().GetField(value.ToString());
