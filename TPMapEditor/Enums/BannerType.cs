32c159a1a9853a91c7adc44566c2b71b6ff12067
Randhomme <randormi@gmail.com>
2025-11-25 12:26:18 +0100

    feat: add proper name attribute to BannerType

--- TPMapEditor/Enums/BannerType.cs
+++ TPMapEditor/Enums/BannerType.cs
@@ -11,8 +11,11 @@ namespace TPMapEditor.Enums
     {
         [Display(Name = "No Banner")]
         NoBanner,
+        [Display(Name = "Banner RedTeam")]
         RedTeam,
+        [Display(Name = "Banner BlueTeam")]
         BlueTeam,
+        [Display(Name = "Banner BlackTeam")]
         BlackTeam
     }
 }
