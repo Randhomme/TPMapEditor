191b5f3ca3b20212a38a85790c2221b201071969
Randhomme <randormi@gmail.com>
2025-12-11 16:56:06 +0100

    feat: make Slider get the focus back by clicking on it

---
+++ TPMapEditor/Styles/SliderTextBoxControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace TPMapEditor.Styles
+{
+    public partial class SliderTextBoxControl : ResourceDictionary
+    {
+        private void Slider_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Slider slider)
+                slider.Focus();
+        }
+    }
+}
