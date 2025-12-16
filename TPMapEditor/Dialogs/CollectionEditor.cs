8026459fccb01a44961a068bd0940be19cc72d46
Randhomme <randormi@gmail.com>
2025-12-16 23:11:43 +0100

    feat: add CollectionEditorControl and Window

---
+++ TPMapEditor/Dialogs/CollectionEditor.cs
@@ -0,0 +1,118 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using TPMapEditor.Data;
+
+namespace TPMapEditor.Dialogs
+{
+    public partial class CollectionEditor<T> : ObservableObject where T : INotifyPropertyChanged
+    {
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(MoveUpObjectCommand))]
+        [NotifyCanExecuteChangedFor(nameof(MoveDownObjectCommand))]
+        private CollectionEditorObjectWrapper<T>? selectedItem;
+
+        private readonly Func<T> factory;
+
+        public ObservableCollection<T> Source { get; }
+        public ObservableCollection<CollectionEditorObjectWrapper<T>> Items { get; }
+
+        public CollectionEditor(ObservableCollection<T> source, Func<T> factory)
+        {
+            Source = source;
+            this.factory = factory;
+            Items = new ObservableCollection<CollectionEditorObjectWrapper<T>>(Source.Select(item => new CollectionEditorObjectWrapper<T>(item)));
+            Items.CollectionChanged += Items_CollectionChanged;
+        }
+
+        private void Items_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems != null)
+                foreach (CollectionEditorObjectWrapper<T> item in e.NewItems)
+                    Source.Add(item.Item);
+
+            if (e.OldItems != null)
+                foreach (CollectionEditorObjectWrapper<T> item in e.OldItems)
+                {
+                    var wrapper = Source.First(w => Equals(w, item.Item));
+                    Source.Remove(wrapper);
+                }
+        }
+
+        [RelayCommand]
+        private void OnAddObject()
+        {
+            var wrapper = new CollectionEditorObjectWrapper<T>(factory());
+            Items.Add(wrapper);
+            SelectedItem = wrapper;
+        }
+
+        [RelayCommand]
+        private void OnRemoveObject()
+        {
+            if (SelectedItem != null)
+                Items.Remove(SelectedItem);
+        }
+
+        [RelayCommand(CanExecute = nameof(CanMoveUpWorldRule))]
+        private void OnMoveUpObject()
+        {
+            var index = Items.IndexOf(SelectedItem!);
+            Items.Move(index, index - 1);
+            MoveUpObjectCommand.NotifyCanExecuteChanged();
+            MoveDownObjectCommand.NotifyCanExecuteChanged();
+        }
+
+        [RelayCommand(CanExecute = nameof(CanMoveDownWorldRule))]
+        private void OnMoveDownObject()
+        {
+            var index = Items.IndexOf(SelectedItem!);
+            Items.Move(index, index + 1);
+            MoveUpObjectCommand.NotifyCanExecuteChanged();
+            MoveDownObjectCommand.NotifyCanExecuteChanged();
+        }
+
+        private bool CanMoveUpWorldRule()
+        {
+            return SelectedItem != null && Items.IndexOf(SelectedItem) > 0;
+        }
+
+        private bool CanMoveDownWorldRule()
+        {
+            return SelectedItem != null && Items.IndexOf(SelectedItem) < Items.Count - 1;
+        }
+    }
+
+    public class CollectionEditorObjectWrapper : ObservableObject { }
+
+    public class CollectionEditorObjectWrapper<T> : CollectionEditorObjectWrapper where T : INotifyPropertyChanged
+    {
+        public T Item { get; }
+
+        public string Display => Item.ToString();
+
+        public CollectionEditorObjectWrapper(T item)
+        {
+            Item = item;
+            Item.PropertyChanged += Item_PropertyChanged;
+        }
+
+        private void Item_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            OnPropertyChanged(nameof(Display));
+        }
+
+        //public override string ToString()
+        //{
+        //    return Display;
+        //}
+    }
+}
---
+++ TPMapEditor/Dialogs/CollectionEditorControl.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TPMapEditor.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for CollectionEditorControl.xaml
+    /// </summary>
+    public partial class CollectionEditorControl : Control
+    {
+        public bool GridOnlyMode
+        {
+            get => (bool)GetValue(GridOnlyModeProperty);
+            set => SetValue(GridOnlyModeProperty, value);
+        }
+
+        public static readonly DependencyProperty GridOnlyModeProperty =
+            DependencyProperty.Register(nameof(GridOnlyMode), typeof(bool), typeof(CollectionEditorControl), new PropertyMetadata(false));
+
+        public IEnumerable Items
+        {
+            get => (IEnumerable)GetValue(ItemsProperty);
+            set => SetValue(ItemsProperty, value);
+        }
+
+        public static readonly DependencyProperty ItemsProperty =
+            DependencyProperty.Register(nameof(Items), typeof(IEnumerable), typeof(CollectionEditorControl));
+
+        public object SelectedItem
+        {
+            get => GetValue(SelectedItemProperty);
+            set => SetValue(SelectedItemProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register(nameof(SelectedItem), typeof(object), typeof(CollectionEditorControl));
+
+        public ICommand AddCommand
+        {
+            get { return (ICommand)GetValue(AddCommandProperty); }
+            set { SetValue(AddCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty AddCommandProperty =
+            DependencyProperty.Register(nameof(AddCommand), typeof(ICommand), typeof(CollectionEditorControl));
+
+        public ICommand RemoveCommand
+        {
+            get { return (ICommand)GetValue(RemoveCommandProperty); }
+            set { SetValue(RemoveCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty RemoveCommandProperty =
+            DependencyProperty.Register(nameof(RemoveCommand), typeof(ICommand), typeof(CollectionEditorControl));
+
+        public ICommand MoveUpCommand
+        {
+            get { return (ICommand)GetValue(MoveUpCommandProperty); }
+            set { SetValue(MoveUpCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty MoveUpCommandProperty =
+            DependencyProperty.Register(nameof(MoveUpCommand), typeof(ICommand), typeof(CollectionEditorControl));
+
+
+        public ICommand MoveDownCommand
+        {
+            get { return (ICommand)GetValue(MoveDownCommandProperty); }
+            set { SetValue(MoveDownCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty MoveDownCommandProperty =
+            DependencyProperty.Register(nameof(MoveDownCommand), typeof(ICommand), typeof(CollectionEditorControl));
+
+    }
+}
