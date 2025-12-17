19a6a4836d51ca06c2a03301078373a405c84f76
Randhomme <randormi@gmail.com>
2025-12-17 18:28:50 +0100

    refactor: move CollectionEditorContext and remove DP from control

--- TPMapEditor/Dialogs/CollectionEditorControl.cs
+++ TPMapEditor/Dialogs/CollectionEditorControl.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,60 +35,205 @@ namespace TPMapEditor.Dialogs
         public static readonly DependencyProperty GridOnlyModeProperty =
             DependencyProperty.Register(nameof(GridOnlyMode), typeof(bool), typeof(CollectionEditorControl), new PropertyMetadata(false));
 
-        public IEnumerable Items
+        //public IEnumerable ItemsSource
+        //{
+        //    get => (IEnumerable)GetValue(ItemsSourceProperty);
+        //    set => SetValue(ItemsSourceProperty, value);
+        //}
+
+        //public static readonly DependencyProperty ItemsSourceProperty =
+        //    DependencyProperty.Register(nameof(ItemsSource), typeof(IEnumerable), typeof(CollectionEditorControl), new PropertyMetadata(null, OnItemsSourceChanged));
+
+        //private static void OnItemsSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        //{
+
+        //}
+
+        //public object SelectedItem
+        //{
+        //    get => GetValue(SelectedItemProperty);
+        //    set => SetValue(SelectedItemProperty, value);
+        //}
+
+        //public static readonly DependencyProperty SelectedItemProperty =
+        //    DependencyProperty.Register(nameof(SelectedItem), typeof(object), typeof(CollectionEditorControl));
+
+        //public ICommand AddCommand
+        //{
+        //    get { return (ICommand)GetValue(AddCommandProperty); }
+        //    set { SetValue(AddCommandProperty, value); }
+        //}
+
+        //public static readonly DependencyProperty AddCommandProperty =
+        //    DependencyProperty.Register(nameof(AddCommand), typeof(ICommand), typeof(CollectionEditorControl));
+
+        //public ICommand RemoveCommand
+        //{
+        //    get { return (ICommand)GetValue(RemoveCommandProperty); }
+        //    set { SetValue(RemoveCommandProperty, value); }
+        //}
+
+        //public static readonly DependencyProperty RemoveCommandProperty =
+        //    DependencyProperty.Register(nameof(RemoveCommand), typeof(ICommand), typeof(CollectionEditorControl));
+
+        //public ICommand MoveUpCommand
+        //{
+        //    get { return (ICommand)GetValue(MoveUpCommandProperty); }
+        //    set { SetValue(MoveUpCommandProperty, value); }
+        //}
+
+        //public static readonly DependencyProperty MoveUpCommandProperty =
+        //    DependencyProperty.Register(nameof(MoveUpCommand), typeof(ICommand), typeof(CollectionEditorControl));
+
+
+        //public ICommand MoveDownCommand
+        //{
+        //    get { return (ICommand)GetValue(MoveDownCommandProperty); }
+        //    set { SetValue(MoveDownCommandProperty, value); }
+        //}
+
+        //public static readonly DependencyProperty MoveDownCommandProperty =
+        //    DependencyProperty.Register(nameof(MoveDownCommand), typeof(ICommand), typeof(CollectionEditorControl));
+
+
+
+        //public Func<INotifyPropertyChanged> Factory
+        //{
+        //    get { return (Func<INotifyPropertyChanged>)GetValue(FactoryProperty); }
+        //    set { SetValue(FactoryProperty, value); }
+        //}
+
+        //// Using a DependencyProperty as the backing store for Factory.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty FactoryProperty =
+        //    DependencyProperty.Register(nameof(Factory), typeof(Func<INotifyPropertyChanged>), typeof(CollectionEditorControl));
+
+
+
+    }
+
+    public partial class CollectionEditorControlContext<T> : ObservableObject where T : INotifyPropertyChanged
+    {
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(MoveUpCommand))]
+        [NotifyCanExecuteChangedFor(nameof(MoveDownCommand))]
+        private CollectionEditorObjectWrapper<T>? selectedItem;
+
+        [ObservableProperty]
+        private bool gridOnlyMode;
+
+        private readonly Func<T> factory;
+
+        public ObservableCollection<T> Source { get; }
+        public ObservableCollection<CollectionEditorObjectWrapper<T>> Items { get; }
+
+        public CollectionEditorControlContext(ObservableCollection<T> source, Func<T> factory, bool gridOnlyMode = false)
         {
-            get => (IEnumerable)GetValue(ItemsProperty);
-            set => SetValue(ItemsProperty, value);
+            Source = source;
+            this.factory = factory;
+            this.gridOnlyMode = gridOnlyMode;
+            Items = new ObservableCollection<CollectionEditorObjectWrapper<T>>(Source.Select(item => new CollectionEditorObjectWrapper<T>(item)));
+            Items.CollectionChanged += Items_CollectionChanged;
         }
 
-        public static readonly DependencyProperty ItemsProperty =
-            DependencyProperty.Register(nameof(Items), typeof(IEnumerable), typeof(CollectionEditorControl));
-
-        public object SelectedItem
+        private void Items_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
-            get => GetValue(SelectedItemProperty);
-            set => SetValue(SelectedItemProperty, value);
+            if (e.NewItems != null)
+                foreach (CollectionEditorObjectWrapper<T> item in e.NewItems)
+                    Source.Add(item.Item);
+
+            if (e.OldItems != null)
+                foreach (CollectionEditorObjectWrapper<T> item in e.OldItems)
+                {
+                    var wrapper = Source.First(w => Equals(w, item.Item));
+                    Source.Remove(wrapper);
+                }
         }
 
-        public static readonly DependencyProperty SelectedItemProperty =
-            DependencyProperty.Register(nameof(SelectedItem), typeof(object), typeof(CollectionEditorControl));
-
-        public ICommand AddCommand
+        [RelayCommand]
+        private void OnAdd()
         {
-            get { return (ICommand)GetValue(AddCommandProperty); }
-            set { SetValue(AddCommandProperty, value); }
+            var wrapper = new CollectionEditorObjectWrapper<T>(factory());
+            Items.Add(wrapper);
+            SelectedItem = wrapper;
         }
 
-        public static readonly DependencyProperty AddCommandProperty =
-            DependencyProperty.Register(nameof(AddCommand), typeof(ICommand), typeof(CollectionEditorControl));
-
-        public ICommand RemoveCommand
+        [RelayCommand]
+        private void OnRemove()
         {
-            get { return (ICommand)GetValue(RemoveCommandProperty); }
-            set { SetValue(RemoveCommandProperty, value); }
+            if (SelectedItem != null)
+            {
+                Items.Remove(SelectedItem);
+                SelectedItem = null;
+            }
         }
 
-        public static readonly DependencyProperty RemoveCommandProperty =
-            DependencyProperty.Register(nameof(RemoveCommand), typeof(ICommand), typeof(CollectionEditorControl));
-
-        public ICommand MoveUpCommand
+        [RelayCommand(CanExecute = nameof(CanMoveUp))]
+        private void OnMoveUp()
         {
-            get { return (ICommand)GetValue(MoveUpCommandProperty); }
-            set { SetValue(MoveUpCommandProperty, value); }
+            var index = Items.IndexOf(SelectedItem!);
+            Items.Move(index, index - 1);
+            MoveUpCommand.NotifyCanExecuteChanged();
+            MoveDownCommand.NotifyCanExecuteChanged();
         }
 
-        public static readonly DependencyProperty MoveUpCommandProperty =
-            DependencyProperty.Register(nameof(MoveUpCommand), typeof(ICommand), typeof(CollectionEditorControl));
-
-
-        public ICommand MoveDownCommand
+        [RelayCommand(CanExecute = nameof(CanMoveDown))]
+        private void OnMoveDown()
         {
-            get { return (ICommand)GetValue(MoveDownCommandProperty); }
-            set { SetValue(MoveDownCommandProperty, value); }
+            var index = Items.IndexOf(SelectedItem!);
+            Items.Move(index, index + 1);
+            MoveUpCommand.NotifyCanExecuteChanged();
+            MoveDownCommand.NotifyCanExecuteChanged();
         }
 
-        public static readonly DependencyProperty MoveDownCommandProperty =
-            DependencyProperty.Register(nameof(MoveDownCommand), typeof(ICommand), typeof(CollectionEditorControl));
+        private bool CanMoveUp()
+        {
+            return SelectedItem != null && Items.IndexOf(SelectedItem) > 0;
+        }
 
+        private bool CanMoveDown()
+        {
+            return SelectedItem != null && Items.IndexOf(SelectedItem) < Items.Count - 1;
+        }
+    }
+
+    public class CollectionEditorObjectWrapper<T> : ObservableObject where T : INotifyPropertyChanged
+    {
+        public T Item { get; }
+
+        public string Display => Item.ToString();
+
+        public CollectionEditorObjectWrapper(T item)
+        {
+            Item = item;
+            Item.PropertyChanged += Item_PropertyChanged;
+        }
+
+        private void Item_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            OnPropertyChanged(nameof(Display));
+        }
+    }
+
+    public static class DataGridColumnRegistry
+    {
+        private static readonly Dictionary<Type, Func<IList<DataGridColumn>>> _map
+            = new();
+
+        public static void Register<T>(Func<IList<DataGridColumn>> factory)
+        {
+            _map[typeof(T)] = factory;
+        }
+
+        public static bool TryGet(Type type, out IList<DataGridColumn> columns)
+        {
+            if (_map.TryGetValue(type, out var factory))
+            {
+                columns = factory();
+                return true;
+            }
+
+            columns = null!;
+            return false;
+        }
     }
 }
