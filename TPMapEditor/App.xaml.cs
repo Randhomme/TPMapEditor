f0bc4baccc419b740bd80e6ff48abe3433983667
Randhomme <randormi@gmail.com>
2025-12-22 16:01:44 +0100

    feat: move the thread language to App.xaml.cs

--- TPMapEditor/App.xaml.cs
+++ TPMapEditor/App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,5 +18,10 @@ namespace TPMapEditor
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+        }
     }
 }
