2571a868f0d4e219433bdb7c6f6f8141bec301b2
Randhomme <randormi@gmail.com>
2025-12-26 13:33:34 +0100

    feat: add BLENDER_DIR to bat script

--- TPMapEditor/render_topdown_1_1.bat
+++ TPMapEditor/render_topdown_1_1.bat
@@ -1,10 +1,11 @@
 @echo off
 
+set BLENDER_DIR=Your/Blender/Folder
 set INPUT_DIR=Your/Input/Folder
 set OUTPUT_DIR=Your/Output/Folder
 
 for %%f in ("%INPUT_DIR%\*.obj") do (
-    blender -b -P render_topdown.py -- "%%f" "%OUTPUT_DIR%\%%~nf.png" 1
+    %BLENDER_DIR%\blender -b -P render_topdown.py -- "%%f" "%OUTPUT_DIR%\%%~nf.png" 1
 )
 
 pause
