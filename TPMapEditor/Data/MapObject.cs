52df1975589c8546f7f876f29521fc6b58fe16f8
Randhomme <randormi@gmail.com>
2025-12-28 17:08:22 +0100

    feat: add CustomObservableValidator class

--- TPMapEditor/Data/MapObject.cs
+++ TPMapEditor/Data/MapObject.cs
@@ -1,34 +1,48 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 using TPMapEditor.Interfaces;
+using TPMapEditor.Utils;
 
 namespace TPMapEditor.Data
 {
-    public abstract partial class MapObject : ObservableValidator { }
-
-    public abstract partial class NamedMapObject : MapObject, INamedMapObject
+    public abstract partial class MapObject : CustomObservableValidator, IMapObject
     {
-        [ObservableProperty]
-        private string name;
-
-        partial void OnNameChanging(string value)
-        {
-            if (string.IsNullOrEmpty(value))
-                throw new ArgumentException($"{GetType().Name} name cannot be null or empty.");
-            if (IsDefaultName(value))
-                throw new ArgumentException($"{value} is already a default value for {GetType().Name}.");
-            if (IsNameTaken(value))
-                throw new ArgumentException($"A {GetType().Name} with the same name in the same group already exists.");
-        }
-
         public WorldMap Map { get; }
-
-        public NamedMapObject(WorldMap map, string name)
+        protected MapObject(WorldMap map)
         {
             Map = map;
+        }
+    }
+
+    public abstract partial class NamedObject : CustomObservableValidator, INamedObject
+    {
+        private string name;
+
+        [Required(ErrorMessage = "The name cannot be empty.", AllowEmptyStrings = false)]
+        [CustomValidation(typeof(NamedObject), nameof(ValidateName))]
+        public string Name
+        {
+            get => name;
+            set => SetAndValidateProperty(ref name, value);
+        }
+
+        public NamedObject(string name)
+        {
             this.name = name;
         }
+
+        public static ValidationResult ValidateName(object value, ValidationContext context)
+        {
+            var instance = (NamedObject)context.ObjectInstance;
+            if (instance.IsNameTaken(value.ToString()))
+                return new ValidationResult($"A {instance.GetType().Name.ToLowerInvariant()} with the same name already exists.");
+            if (instance.IsDefaultName(value.ToString()))
+                return new ValidationResult($"A {value} is already a default name for {instance.GetType().Name}.");
+            return ValidationResult.Success;
+        }        
 
         protected abstract bool IsNameTaken(string name);
 
@@ -37,7 +51,7 @@ namespace TPMapEditor.Data
             return false;
         }
 
-        public static string GenerateName(string prefix, IEnumerable<NamedMapObject> collection)
+        public static string GenerateName(string prefix, IEnumerable<INamedObject> collection)
         {
             var c = 0;
             foreach (var namedMapObject in collection)
@@ -60,10 +74,23 @@ namespace TPMapEditor.Data
         }
     }
 
+    public abstract partial class NamedMapObject : NamedObject, IMapObject
+    {
+        public WorldMap Map { get; }
+        protected NamedMapObject(WorldMap map, string name) : base(name)
+        {
+            Map = map;
+        }
+    }
+
     public abstract partial class SelectableMapObject : MapObject, ISelectableMapObject
     {
         [ObservableProperty]
         private bool isSelected, isLastSelected, isShownOnUi = true;
+
+        protected SelectableMapObject(WorldMap map) : base(map)
+        {
+        }
     }
 
     public abstract partial class SelectableNamedMapObject : NamedMapObject, ISelectableMapObject
---
+++ TPMapEditor/Utils/CustomObservableValidator.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMapEditor.Utils
+{
+    /// <summary>
+    /// An observable validator that triggers exceptions instead of binding errors, using Validator.ValidateProperty and Validator.ValidateObject
+    /// </summary>
+    public class CustomObservableValidator : ObservableObject
+    {
+        protected void ValidateProperty<T>(T value, [CallerMemberName] string? propertyName = null)
+        {
+            Validator.ValidateProperty(value, new(this) { MemberName = propertyName });
+        }
+
+        protected void ValidateAllProperties()
+        {
+            Validator.ValidateObject(this, new(this), true);
+        }
+
+        protected void SetAndValidateProperty<T>(ref T field, T newValue, [CallerMemberName]string? propertyName = null)
+        {
+            ValidateProperty(newValue, propertyName);
+            SetProperty(ref field, newValue, propertyName);
+        }
+    }
+}
