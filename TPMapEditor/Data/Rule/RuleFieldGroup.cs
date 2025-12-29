6aeccc9eef9e8e401c21fd8a1165de907a96dd84
Randhomme <randormi@gmail.com>
2025-12-29 15:10:22 +0100

    feat: add ToString override in RuleFieldGroup

--- TPMapEditor/Data/Rule/RuleFieldGroup.cs
+++ TPMapEditor/Data/Rule/RuleFieldGroup.cs
@@ -5,5 +5,10 @@
         public RuleFieldGroup(WorldMap map, string? realLabel, string? label, Group value, bool isOptional = false, string? optionalLabel = null, bool isShown = true) : base(map, realLabel, label, value, isOptional, optionalLabel, isShown)
         {
         }
+
+        public override string ToString()
+        {
+            return $"{RealLabel} '{Value?.ToString() ?? Group.DefaultName}'";
+        }
     }
 }
