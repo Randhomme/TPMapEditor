dae38152e18b70c218a25ff5dd352a16ee475556
Randhomme <randormi@gmail.com>
2025-12-29 09:40:42 +0100

    feat: add a way to sync observable collections

---
+++ TPMapEditor/Utils/ObservableCollectionExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMapEditor.Utils
+{
+    public static class ObservableCollectionExtensions
+    {
+        public static void SynchronizeFrom<T>(this ObservableCollection<T> target, ObservableCollection<T> source, NotifyCollectionChangedEventArgs e, IReadOnlyList<T>? prefixItems = null)
+        {
+            prefixItems ??= Array.Empty<T>();
+
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    if (e.NewItems != null)
+                        foreach (T item in e.NewItems)
+                            target.Add(item);
+                    break;
+
+                case NotifyCollectionChangedAction.Remove:
+                    if (e.OldItems != null)
+                        foreach (T item in e.OldItems)
+                            target.Remove(item);
+                    break;
+
+                case NotifyCollectionChangedAction.Replace:
+                    if (e.OldItems != null)
+                        foreach (T item in e.OldItems)
+                            target.Remove(item);
+                    if (e.NewItems != null)
+                        foreach (T item in e.NewItems)
+                            target.Add(item);
+                    break;
+
+                case NotifyCollectionChangedAction.Reset:
+                    target.Clear();
+
+                    foreach (var item in prefixItems)
+                        target.Add(item);
+
+                    foreach (var item in source)
+                        target.Add(item);
+                    break;
+            }
+        }
+    }
+}
