ff7b0d5640696aff578e7691db690e4847b61227
Randhomme <randormi@gmail.com>
2026-01-05 21:06:26 +0100

    feat: extend ISelectionService for MultiPointMapObject

---
+++ TPMapEditor/Services/IMultiPointMapObjectSelectionService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPMapEditor.Interfaces;
+
+namespace TPMapEditor.Services
+{
+    public interface IMultiPointMapObjectSelectionService<T1, T2> : ISelectionService<T1> where T1 : IMultiPointMapObject<T2> where T2 : ISelectableMapObject
+    {
+        public void SelectAndMakeLastSelectedWithoutPoints(T1 mapObject);
+
+        public void RemoveFromSelectionWithoutPoints(T1 mapObject);
+    }
+}
