14d51225a6f8b18f76d23708331b86d81d50f5fe
Randhomme <randormi@gmail.com>
2026-01-05 21:06:01 +0100

    feat: add new ISelectionService implementation

---
+++ TPMapEditor/Services/Implementations/MultiPointMapObjectSelectionService.cs
@@ -0,0 +1,57 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TPMapEditor.Interfaces;
+
+namespace TPMapEditor.Services.Implementations
+{
+    public class MultiPointMapObjectSelectionService<T1, T2> : SelectionService<T1>, IMultiPointMapObjectSelectionService<T1, T2> where T1 : IMultiPointMapObject<T2> where T2 : ISelectableMapObject
+    {
+        private readonly ISelectionService<T2> pointSelectionService;
+
+        public MultiPointMapObjectSelectionService(ISelectionService<T2> pointSelectionService) : base()
+        {
+            this.pointSelectionService = pointSelectionService;
+        }
+
+        public override void Select(T1 mapObject)
+        {
+            base.Select(mapObject);
+            foreach (var item in mapObject.Points)
+            {
+                pointSelectionService.AddToSelection(item);
+            }
+        }
+
+        public override void Unselect(T1 mapObject)
+        {
+            base.Unselect(mapObject);
+            foreach (var item in mapObject.Points)
+            {
+                pointSelectionService.RemoveFromSelection(item);
+            }
+        }
+
+        public void SelectAndMakeLastSelectedWithoutPoints(T1 mapObject)
+        {
+            var selectedPoints = mapObject.Points.Where((p) => p.IsSelected).ToArray();
+            AddToSelection(mapObject);
+            foreach (var p in mapObject.Points)
+            {
+                if (!selectedPoints.Contains(p))
+                    pointSelectionService.RemoveFromSelection(p);
+            }
+            MakeLastSelected(mapObject);
+        }
+
+        public void RemoveFromSelectionWithoutPoints(T1 mapObject)
+        {
+            if (!mapObject.Points.Any((p) => p.IsSelected))
+                base.RemoveFromSelection(mapObject);
+        }
+    }
+}
