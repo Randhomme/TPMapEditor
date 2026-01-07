2d986fff49bac198849a092c23f1f4e597109d21
Randhomme <randormi@gmail.com>
2026-01-07 14:43:20 +0100

    feat: add new Window for selection transformation

---
+++ TPMapEditor/Dialogs/SelectionTransformWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using TPMapEditor.ViewModel.SelectionTransform;
+
+namespace TPMapEditor.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for SelectionTransformWindow.xaml
+    /// </summary>
+    public partial class SelectionTransformWindow : Window
+    {
+        private readonly SelectionTransformBaseViewModel vm;
+        public SelectionTransformWindow(Window owner, string title, SelectionTransformBaseViewModel vm)
+        {
+            this.Owner = owner;
+            this.Title = title;
+            this.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            this.DataContext = this.vm = vm;
+            InitializeComponent();
+            this.Closed += (s, e) =>
+            {
+                if (vm.ShouldCommitCommand)
+                    vm.CommitCommand();
+                else
+                    vm.CancelCommand();
+            };
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            vm.ShouldCommitCommand = true;
+            this.Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            vm.ShouldCommitCommand = false;
+            this.Close();
+        }
+    }
+}
