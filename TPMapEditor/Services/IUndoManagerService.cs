329f5a29d832b5750b7c1e21cdb6833212c8f39b
Randhomme <randormi@gmail.com>
2026-01-07 14:42:20 +0100

    feat: add UndoManager service

---
+++ TPMapEditor/Services/IUndoManagerService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPMapEditor.Interfaces;
+
+namespace TPMapEditor.Services
+{
+    public interface IUndoManagerService : INotifyPropertyChanged
+    {
+        public bool CanUndo { get; }
+        public bool CanRedo { get; }
+
+        /// <summary>
+        /// Adds an action that can be undone
+        /// </summary>
+        /// <param name="action"></param>
+        public void Push(IUndoableMapCommand action);
+
+        /// <summary>
+        /// Undo the last action
+        /// </summary>
+        public void Undo();
+
+        /// <summary>
+        /// Redo the last action
+        /// </summary>
+        public void Redo();
+    }
+}
---
+++ TPMapEditor/Services/Implementations/UndoManagerService.cs
@@ -0,0 +1,57 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPMapEditor.Interfaces;
+
+namespace TPMapEditor.Services.Implementations
+{
+    public class UndoManagerService : ObservableObject, IUndoManagerService
+    {
+        private readonly int _max;
+        private readonly Stack<IUndoableMapCommand> _undo = new();
+        private readonly Stack<IUndoableMapCommand> _redo = new();
+
+        public bool CanUndo { get => _undo.Count > 0; }
+        public bool CanRedo { get => _redo.Count > 0; }
+
+        public UndoManagerService(int max = 10)
+        {
+            _max = max;
+        }
+
+        public void Push(IUndoableMapCommand cmd)
+        {
+            _undo.Push(cmd);
+            _redo.Clear();
+
+            if (_undo.Count > _max)
+                _undo.Pop();
+
+            OnPropertyChanged(nameof(CanUndo));
+            OnPropertyChanged(nameof(CanRedo));
+        }
+
+        public void Undo()
+        {
+            if (_undo.Count == 0) return;
+            var cmd = _undo.Pop();
+            cmd.Undo();
+            _redo.Push(cmd);
+            OnPropertyChanged(nameof(CanUndo));
+            OnPropertyChanged(nameof(CanRedo));
+        }
+
+        public void Redo()
+        {
+            if (_redo.Count == 0) return;
+            var cmd = _redo.Pop();
+            cmd.Redo();
+            _undo.Push(cmd);
+            OnPropertyChanged(nameof(CanUndo));
+            OnPropertyChanged(nameof(CanRedo));
+        }
+    }
+}
