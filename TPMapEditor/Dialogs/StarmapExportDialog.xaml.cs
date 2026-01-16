a7c39a22bedf148ec0b577f6a6f450376cf99d8c
Randhomme <randormi@gmail.com>
2026-01-16 21:04:03 +0100

    feat: add export function

--- TPMapEditor/Dialogs/StarmapExportDialog.xaml.cs
+++ TPMapEditor/Dialogs/StarmapExportDialog.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System.Drawing;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using TPMapEditor.ViewModel;
 
 namespace TPMapEditor.Dialogs
 {
@@ -23,6 +15,28 @@ namespace TPMapEditor.Dialogs
         public StarmapExportDialog(Window owner, string title) : base(owner, title)
         {
             InitializeComponent();
+        }
+
+        private void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+            var sfd = new SaveFileDialog()
+            {
+                DefaultExt = ".png",
+                Filter = "PNG image (.png)|*.png",
+                Title = "Export the starmap image",
+            };
+            if (sfd.ShowDialog(this) == true)
+            {
+                var rtb = new RenderTargetBitmap(512, 512, 96, 96, PixelFormats.Pbgra32);
+                rtb.Render(MapViewbox);
+                var encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(rtb));
+                using (var fs = File.Create(sfd.FileName))
+                {
+                    encoder.Save(fs);
+                    MessageBox.Show("Starmap image exported !", "Starmap image exported", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
         }
     }
 }
