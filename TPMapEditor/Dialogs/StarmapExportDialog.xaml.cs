0f6cab1fd092fb0748489693baeb29cc2d7497e2
Randhomme <randormi@gmail.com>
2026-01-16 14:34:27 +0100

    feat: add starmap preview

---
+++ TPMapEditor/Dialogs/StarmapExportDialog.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TPMapEditor.ViewModel;
+
+namespace TPMapEditor.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for StarmapExportWindow.xaml
+    /// </summary>
+    public partial class StarmapExportDialog : DialogWindow
+    {
+        public StarmapExportDialog(Window owner, string title) : base(owner, title)
+        {
+            InitializeComponent();
+        }
+    }
+}
