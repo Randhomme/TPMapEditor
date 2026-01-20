37979a6334f5c4b6e99c78d108edfe5c36082c41
Randhomme <randormi@gmail.com>
2026-01-20 15:32:11 +0100

    feat: add Nebula collection to StarmapExportViewModel

--- TPMapEditor/ViewModel/StarmapExportViewModel.cs
+++ TPMapEditor/ViewModel/StarmapExportViewModel.cs
@@ -14,25 +14,28 @@ namespace TPMapEditor.ViewModel
     public partial class StarmapExportViewModel : ObservableObject
     {
         public WorldMap Map { get; }
-
         public double NegativeWorldBuffer { get => -Map.WorldBuffer; }
-
         public double BorderThickness { get; private set; }
+        public double NebulaBlurRadiue { get; private set; }
 
         public ICollectionView Asteroids { get; }
         public ICollectionView BlackHoles { get; }
         public ICollectionView Islands { get; }
+        public ICollectionView Nebulas { get; }
 
         public StarmapExportViewModel(WorldMap map)
         {
             this.Map = map;
             BorderThickness = 5 * Map.Size / 512;
+            NebulaBlurRadiue = 15 * Map.Size / 512;
             Islands = new CollectionViewSource() { Source = map.WorldObjects }.View;
             Islands.Filter = IsWorldObjectIsland;
             BlackHoles = new CollectionViewSource() { Source = map.WorldObjects }.View;
             BlackHoles.Filter = IsWorldObjectBlackHole;
             Asteroids = new CollectionViewSource() { Source = map.WorldObjects }.View;
             Asteroids.Filter = IsWorldObjectAsteroid;
+            Nebulas = new CollectionViewSource() { Source = map.Nebulas }.View;
+            Nebulas.Filter = IsNebulaNebula;
         }
 
         private bool IsWorldObjectIsland(object o)
@@ -58,6 +61,15 @@ namespace TPMapEditor.ViewModel
             if(o is WorldObject worldObject)
             {
                 return worldObject.Type.CustomInfoDefinition == CustomInfoDefinition.AsteroidCustomInfoFactory;
+            }
+            return false;
+        }
+
+        private bool IsNebulaNebula(object o)
+        {
+            if(o is Nebula nebula)
+            {
+                return nebula.NebulaPointSet != WorldPointSet.DefaultWorldPointSet;
             }
             return false;
         }
