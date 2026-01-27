7c831b3d2187349fd90b00c209438eb84865a83d
Randhomme <randormi@gmail.com>
2026-01-27 11:44:29 +0100

    feat: use outline in StarmapExportViewModel

--- TPMapEditor/ViewModel/StarmapExportViewModel.cs
+++ TPMapEditor/ViewModel/StarmapExportViewModel.cs
@@ -4,38 +4,63 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Windows.Media;
 using TPMapEditor.Data;
 using TPMapEditor.Enums.WorldObjectDefinition;
+using TPMapEditor.Utils;
 
 namespace TPMapEditor.ViewModel
 {
     public partial class StarmapExportViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private bool isReady = false;
+        private Brush islandBrush;
+        private Brush asteroidBrush;
+        private Color outlineColor;
+
         public WorldMap Map { get; }
         public double NegativeWorldBuffer { get => -Map.WorldBuffer; }
-        public double BorderThickness { get; private set; }
-        public double EtheriumCurrentThickness { get; private set; }
-        public double NebulaBlurRadius { get; private set; }
-
+        public double BorderThickness { get; }
+        public double NegativeBorderThickness { get; }
+        public double EtheriumCurrentThickness { get; }
+        public double NebulaBlurRadius { get; }
         public ICollectionView Asteroids { get; }
         public ICollectionView BlackHoles { get; }
         public ICollectionView Islands { get; }
         public ICollectionView Nebulas { get; }
         public ICollectionView EtheriumCurrents { get; }
 
+        private readonly SemaphoreSlim _semaphore = new(4); // CPU limit
+
         public StarmapExportViewModel(WorldMap map)
         {
+            islandBrush = new RadialGradientBrush(new()
+            {
+                new(Colors.Cyan, 0.25),
+                new(Colors.DarkCyan, 0.75)
+            })
+            {
+                RadiusX = 1,
+                RadiusY = 1,
+                Center = new(0.5,0.5)
+            };
+            asteroidBrush = new SolidColorBrush(Colors.Lime);
+            outlineColor = Colors.White;
             this.Map = map;
-            BorderThickness = 5 * Map.Size / 512;
-            NebulaBlurRadius = 15 * Map.Size / 512;
-            EtheriumCurrentThickness = 10 * Map.Size / 512;
-            Islands = new CollectionViewSource() { Source = map.WorldObjects }.View;
+            BorderThickness = 1.25 * (Map.Size - Map.WorldBuffer) / 512.0;
+            NegativeBorderThickness = -BorderThickness;
+            NebulaBlurRadius = 15 * (Map.Size - Map.WorldBuffer) / 512;
+            EtheriumCurrentThickness = 11 * (Map.Size - Map.WorldBuffer) / 512;
+            IList<StarmapWorldObjectViewModel> worldObjectViewModels = new List<StarmapWorldObjectViewModel>(map.WorldObjects.Select((w) => new StarmapWorldObjectViewModel(w)));
+            Islands = new CollectionViewSource() { Source = worldObjectViewModels }.View;
             Islands.Filter = IsWorldObjectIsland;
-            BlackHoles = new CollectionViewSource() { Source = map.WorldObjects }.View;
+            BlackHoles = new CollectionViewSource() { Source = worldObjectViewModels }.View;
             BlackHoles.Filter = IsWorldObjectBlackHole;
-            Asteroids = new CollectionViewSource() { Source = map.WorldObjects }.View;
+            Asteroids = new CollectionViewSource() { Source = worldObjectViewModels }.View;
             Asteroids.Filter = IsWorldObjectAsteroid;
             Nebulas = new CollectionViewSource() { Source = map.Nebulas }.View;
             Nebulas.Filter = IsNebulaNebula;
@@ -44,27 +69,27 @@ namespace TPMapEditor.ViewModel
 
         private bool IsWorldObjectIsland(object o)
         {
-            if(o is WorldObject worldObject)
+            if(o is StarmapWorldObjectViewModel worldObject)
             {
-                return worldObject.Type.CustomInfoDefinition == CustomInfoDefinition.IslandCustomInfoFactory;
+                return worldObject.CustomInfo == CustomInfoDefinition.IslandCustomInfoFactory;
             }
             return false;
         }
 
         private bool IsWorldObjectBlackHole(object o)
         {
-            if(o is WorldObject worldObject)
+            if(o is StarmapWorldObjectViewModel worldObject)
             {
-                return worldObject.Type.CustomInfoDefinition == CustomInfoDefinition.BlackHoleCustomInfoFactory;
+                return worldObject.CustomInfo == CustomInfoDefinition.BlackHoleCustomInfoFactory;
             }
             return false;
         }
 
         private bool IsWorldObjectAsteroid(object o)
         {
-            if(o is WorldObject worldObject)
+            if(o is StarmapWorldObjectViewModel worldObject)
             {
-                return worldObject.Type.CustomInfoDefinition == CustomInfoDefinition.AsteroidCustomInfoFactory;
+                return worldObject.CustomInfo == CustomInfoDefinition.AsteroidCustomInfoFactory;
             }
             return false;
         }
@@ -76,6 +101,72 @@ namespace TPMapEditor.ViewModel
                 return nebula.NebulaPointSet != WorldPointSet.DefaultWorldPointSet;
             }
             return false;
+        }
+
+        public async Task ProcessIslandsAndAsteroidsImages()
+        {
+            var islands = Islands.Cast<StarmapWorldObjectViewModel>().ToList();
+            var asteroids = Asteroids.Cast<StarmapWorldObjectViewModel>().ToList();
+            var islandsTasks = islands.Select(ProcessIslandsAsync);
+            var asteroidsTasks = asteroids.Select(ProcessAsteroidsAsync);
+            await Task.WhenAll(islandsTasks);
+            await Task.WhenAll(asteroidsTasks);
+        }
+
+        private async Task ProcessIslandsAsync(StarmapWorldObjectViewModel item)
+        {
+            await _semaphore.WaitAsync();
+            try
+            {
+                var original = item.OriginalImage;
+                var gradientImage = BitmapStarmapTransform.ApplyGradient(original, islandBrush);
+                if (gradientImage.CanFreeze)
+                    gradientImage.Freeze();
+
+                var mapSize = Map.Size > Map.WorldBuffer ? Map.Size - Map.WorldBuffer : Map.Size;
+                var outlineThickness = BorderThickness + 10 * Math.Max(gradientImage.PixelWidth, gradientImage.PixelHeight) / (mapSize);
+                var result = await Task.Run(() =>
+                {
+                    var temp = BitmapStarmapTransform.GenerateOutline(gradientImage, outlineColor, outlineThickness);
+                    if (temp.CanFreeze)
+                        temp.Freeze();
+                    return temp;
+                });
+
+                item.StarmapImage = result;
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        private async Task ProcessAsteroidsAsync(StarmapWorldObjectViewModel item)
+        {
+            await _semaphore.WaitAsync();
+            try
+            {
+                var original = item.OriginalImage;
+                var gradientImage = BitmapStarmapTransform.ApplyGradient(original, asteroidBrush);
+                if (gradientImage.CanFreeze)
+                    gradientImage.Freeze();
+
+                var mapSize = Map.Size > Map.WorldBuffer ? Map.Size - Map.WorldBuffer : Map.Size;
+                var outlineThickness = BorderThickness + 10 * Math.Max(gradientImage.PixelWidth, gradientImage.PixelHeight) / (mapSize);
+                var result = await Task.Run(() =>
+                {
+                    var temp = BitmapStarmapTransform.GenerateOutline(gradientImage, outlineColor, outlineThickness);
+                    if (temp.CanFreeze)
+                        temp.Freeze();
+                    return temp;
+                });
+
+                item.StarmapImage = result;
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
         }
     }
 }
---
+++ TPMapEditor/ViewModel/StarmapWorldObjectViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using TPMapEditor.Data;
+using TPMapEditor.Enums.WorldObjectDefinition;
+
+namespace TPMapEditor.ViewModel
+{
+    public partial class StarmapWorldObjectViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private BitmapSource starmapImage;
+
+        public double X { get; }
+        public double Y { get; }
+        public double ZRotation { get; }
+        public double ZIndex { get; }
+        public Point Pivot { get; }
+        public BitmapSource OriginalImage { get; }
+        public CustomInfoDefinition CustomInfo { get; }
+        public StarmapWorldObjectViewModel(WorldObject worldObject)
+        {
+            X = worldObject.X;
+            Y = worldObject.Y;
+            ZRotation = worldObject.ZRotation;
+            ZIndex = worldObject.ZIndex;
+            Pivot = worldObject.Type.Pivot;
+            OriginalImage = starmapImage = worldObject.Type.Image;
+            CustomInfo = worldObject.Type.CustomInfoDefinition;
+        }
+    }
+}
