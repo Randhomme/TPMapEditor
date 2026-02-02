5d03bb5f69d79813429b2c8de21c279255044ced
Randhomme <randormi@gmail.com>
2026-02-02 16:45:23 +0100

    feat: add copy and paste command for CollectionEditorViewModel

--- TPMapEditor/ViewModel/CollectionEditorViewModel.cs
+++ TPMapEditor/ViewModel/CollectionEditorViewModel.cs
@@ -1,23 +1,57 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using TPMapEditor.Interfaces;
+using TPMapEditor.Services;
 
 namespace TPMapEditor.ViewModel
 {
-    public class CollectionEditorViewModel : ObservableObject
+    public partial class CollectionEditorViewModel<T> : ObservableObject where T : ISelectableMapObject
     {
-        public IEnumerable<object> ItemsSource { get; }
+        private bool canPaste = false;
+        private readonly ICopyPasteService copyPasteService;
+        private readonly ObservableCollection<T> selectedItems = new();
+        public ICollection<T> ItemsSource { get; }
+        public ICollection<T> SelectedItems { get => selectedItems; }
         public Func<object> Factory { get; }
         public bool GridOnlyMode { get; }
 
-        public CollectionEditorViewModel(IEnumerable<object> itemSource, Func<object> factory, bool gridOnlyMode = false)
+        public CollectionEditorViewModel(ICollection<T> itemSource, Func<object> factory, ICopyPasteService copyPasteService, bool gridOnlyMode = false)
         {
             this.ItemsSource = itemSource;
             this.Factory = factory;
+            this.copyPasteService = copyPasteService;
             this.GridOnlyMode = gridOnlyMode;
+            selectedItems.CollectionChanged += (s, e) =>
+            {
+                CopyCommand.NotifyCanExecuteChanged();
+            };
+            copyPasteService.ClearClipboard();
+            canPaste = false;
         }
+
+        [RelayCommand(CanExecute = nameof(CanCopy))]
+        private void OnCopy()
+        {
+            copyPasteService.Copy(SelectedItems);
+            canPaste = true;
+            PasteCommand.NotifyCanExecuteChanged();
+        }
+
+        [RelayCommand(CanExecute = nameof(CanPaste))]
+        private void OnPaste()
+        {
+            var pastedItems = copyPasteService.Paste<T>();
+            foreach (var item in pastedItems)
+            {
+                ItemsSource.Add(item);
+            }
+        }
+
+        private bool CanCopy() => SelectedItems.Count > 0;
+
+        private bool CanPaste() => canPaste;
     }
 }
---
+++ TPMapEditor/ViewModel/TeamEditorViewModel.cs
@@ -0,0 +1,88 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using TPMapEditor.Data;
+using TPMapEditor.Services;
+
+namespace TPMapEditor.ViewModel
+{
+    public partial class TeamEditorViewModel : ObservableObject
+    {
+        private bool canPasteSelectableTeams = false;
+        private bool canPasteInGameTeams = false;
+        private readonly ICopyPasteService copyPasteService;
+        private readonly ObservableCollection<Team> selectedSelectableTeams = new();
+        private readonly ObservableCollection<Team> selectedInGameTeams = new();
+        public ICollection<Team> SelectableTeams { get; }
+        public ICollection<Team> InGameTeams { get; }
+        public ICollection<Team> SelectedSelectableTeams { get => selectedSelectableTeams; }
+        public ICollection<Team> SelectedInGameTeams { get => selectedInGameTeams; }
+        public Func<object> Factory { get; }
+        public bool GridOnlyMode { get; }
+
+        public TeamEditorViewModel(ICollection<Team> selectableTeams, ICollection<Team> inGameTeams, Func<object> factory, ICopyPasteService copyPasteService, bool gridOnlyMode = true)
+        {
+            this.SelectableTeams = selectableTeams;
+            this.InGameTeams = inGameTeams;
+            this.Factory = factory;
+            this.copyPasteService = copyPasteService;
+            this.GridOnlyMode = gridOnlyMode;
+            selectedSelectableTeams.CollectionChanged += (s, e) =>
+            {
+                CopySelectableTeamsCommand.NotifyCanExecuteChanged();
+            };
+            selectedInGameTeams.CollectionChanged += (s, e) =>
+            {
+                CopyInGameTeamsCommand.NotifyCanExecuteChanged();
+            };
+            copyPasteService.ClearClipboard();
+        }
+
+
+        [RelayCommand(CanExecute = nameof(CanCopySelectableTeams))]
+        private void OnCopySelectableTeams()
+        {
+            copyPasteService.Copy(SelectedSelectableTeams);
+            canPasteSelectableTeams = true;
+            PasteSelectableTeamsCommand.NotifyCanExecuteChanged();
+        }
+
+        [RelayCommand(CanExecute = nameof(CanPasteSelectableTeams))]
+        private void OnPasteSelectableTeams()
+        {
+            var pastedItems = copyPasteService.Paste<Team>();
+            foreach (var item in pastedItems)
+            {
+                SelectableTeams.Add(item);
+            }
+        }
+
+        [RelayCommand(CanExecute = nameof(CanCopyInGameTeams))]
+        private void OnCopyInGameTeams()
+        {
+            copyPasteService.Copy(SelectedInGameTeams);
+            canPasteInGameTeams = true;
+            PasteInGameTeamsCommand.NotifyCanExecuteChanged();
+        }
+
+        [RelayCommand(CanExecute = nameof(CanPasteInGameTeams))]
+        private void OnPasteInGameTeams()
+        {
+            var pastedItems = copyPasteService.Paste<Team>();
+            foreach (var item in pastedItems)
+            {
+                InGameTeams.Add(item);
+            }
+        }
+
+        private bool CanCopySelectableTeams() => SelectedSelectableTeams.Count > 0;
+
+        private bool CanPasteSelectableTeams() => canPasteSelectableTeams;
+
+        private bool CanCopyInGameTeams() => SelectedInGameTeams.Count > 0;
+
+        private bool CanPasteInGameTeams() => canPasteInGameTeams;
+    }
+}
