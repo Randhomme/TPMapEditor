7c3c0ccd518c7097ecf9f8278bc90bc84dfae74c
Randhomme <randormi@gmail.com>
2026-02-05 23:11:43 +0100

    fix: update selectable world object types in WorldObjectTemplate

---
+++ TPMapEditor/Converter/SelectableWorldObjectTypeConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+using TPMapEditor.Data;
+
+namespace TPMapEditor.Converter
+{
+    public class SelectableWorldObjectTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is IEnumerable<WorldObjectType> worldObjectTypes)
+            {
+                return worldObjectTypes.Where((wot) => WorldObjectType.IsSelectableWorldObjectType(wot));
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
