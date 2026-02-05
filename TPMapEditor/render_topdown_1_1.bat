2ffbb2a735e4b50ece51532e2fd4cdb638e16a95
Randhomme <randormi@gmail.com>
2026-02-05 19:00:18 +0100

    fix: add double quotes to blender full path in command

--- TPMapEditor/render_topdown_1_1.bat
+++ TPMapEditor/render_topdown_1_1.bat
@@ -5,7 +5,7 @@ set INPUT_DIR=Your/Input/Folder
 set OUTPUT_DIR=Your/Output/Folder
 
 for %%f in ("%INPUT_DIR%\*.obj") do (
-    %BLENDER_DIR%\blender -b -P render_topdown.py -- "%%f" "%OUTPUT_DIR%\%%~nf.png" 1
+    "%BLENDER_DIR%\blender" -b -P render_topdown.py -- "%%f" "%OUTPUT_DIR%\%%~nf.png" 1
 )
 
 pause
