c0f797ba5803c50297f2de6117fc113097fffc3c
Randhomme <randormi@gmail.com>
2026-04-19 12:43:54 +0200

    feat: add IColoredMapObject interface and data template

---
+++ TPMapEditor/DataTemplate/ColoredMapObjectTemplate.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using TPMapEditor.Dialogs;
+using TPMapEditor.Interfaces;
+
+namespace TPMapEditor.DataTemplate
+{
+    public partial class ColoredMapObjectTemplate
+    {
+        private void EditColor_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is FrameworkElement element && element.DataContext is IColoredMapObject coloredMapObject)
+            {
+                var cp = new ColorPicker(Window.GetWindow(element), "Edit color", coloredMapObject.Color);
+                if (cp.ShowDialog() == true)
+                    coloredMapObject.Color = cp.NewColor;
+            }
+        }
+    }
+}
