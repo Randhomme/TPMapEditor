a1e72fd991d44f7ed62b9b9efdc381da3a040eee
Randhomme <randormi@gmail.com>
2026-04-20 15:55:14 +0200

    fix: remove useless using statements

--- TPMapEditor/Utils/CustomObservableValidator.cs
+++ TPMapEditor/Utils/CustomObservableValidator.cs
@@ -1,11 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TPMapEditor.Utils
 {
