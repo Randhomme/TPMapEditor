5918eb1435a679de5e5ddcbf57050d80852bdeda
Randhomme <randormi@gmail.com>
2026-07-08 23:41:11 +0200

    fix: add world point magnitude to multi edit

--- TPMapEditor/ViewModel/MultiWorldPointViewModel.cs
+++ TPMapEditor/ViewModel/MultiWorldPointViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
 using TPMapEditor.Data;
 using TPMapEditor.Services;
 
@@ -6,8 +7,28 @@ namespace TPMapEditor.ViewModel
 {
     public partial class MultiWorldPointViewModel : MultiRotatableMapObjectViewModel<WorldPoint>
     {
+        [ObservableProperty]
+        private double magnitude;
+
         public MultiWorldPointViewModel(IEnumerable<WorldPoint> selectedMapObjects, IUndoManagerService undoManagerService) : base(selectedMapObjects, undoManagerService)
         {
+        }
+
+        protected override void UpdateFromMapObject_Internal(WorldPoint mapObject)
+        {
+            base.UpdateFromMapObject_Internal(mapObject);
+            Magnitude = mapObject.Magnitude;
+        }
+
+        partial void OnMagnitudeChanged(double value)
+        {
+            if (UseUpdateCommands)
+            {
+                foreach (var item in selectedMapObjects)
+                {
+                    item.Magnitude = Magnitude;
+                }
+            }
         }
     }
 }
