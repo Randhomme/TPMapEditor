05836d477b1bc808df78e76d154a49a1cc50b5b5
Randhomme <randormi@gmail.com>
2026-08-05 17:40:15 +0200

    feat: allow game headers settings for each tp game

--- TPMapEditor/Settings/AppSettings.cs
+++ TPMapEditor/Settings/AppSettings.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows;
@@ -27,17 +28,33 @@ namespace TPMapEditor.Settings
         public static IList<string> Meshes { get; } = new List<string>();
         public static IList<string> DialogueFilesList { get; } = new List<string>();
         public static IList<string> HudTexturesList { get; } = new List<string>();
-        public ObservableCollection<GameHeadersFile> TPTeamNames { get; } = new();
-        public ObservableCollection<GameHeadersFile> TPSpeechEvents { get; } = new();
-        public ObservableCollection<GameHeadersFile> TPSpeakerNames { get; } = new();
-        public ObservableCollection<GameHeadersFile> TPShipNames { get; } = new();
-        public ObservableCollection<GameHeadersFile> TPInGameMessages { get; } = new();
-        public ObservableCollection<GameHeadersFile> TPJournalTitles { get; } = new();
-        public ObservableCollection<GameHeadersFile> TPObjectiveTasks { get; } = new();
-        public ObservableCollection<GameHeadersFile> TPSpeechEventsJournals { get; } = new();
-        public ObservableCollection<GameHeadersFile> TPMapTextItems { get; } = new();
-        public ObservableCollection<GameHeadersFile> TPWorldNames { get; } = new();
-        public ObservableCollection<GameHeadersFile> TPWorldDescriptions { get; } = new();
+        [XmlIgnore]
+        public ObservableCollection<GameHeadersFile> TPTeamNames { get => currentStringSettings.TPTeamNames; }
+        [XmlIgnore]
+        public ObservableCollection<GameHeadersFile> TPSpeechEvents { get => currentStringSettings.TPSpeechEvents; }
+        [XmlIgnore]
+        public ObservableCollection<GameHeadersFile> TPSpeakerNames { get => currentStringSettings.TPSpeakerNames; }
+        [XmlIgnore]
+        public ObservableCollection<GameHeadersFile> TPShipNames { get => currentStringSettings.TPShipNames; }
+        [XmlIgnore]
+        public ObservableCollection<GameHeadersFile> TPInGameMessages { get => currentStringSettings.TPInGameMessages; }
+        [XmlIgnore]
+        public ObservableCollection<GameHeadersFile> TPJournalTitles { get => currentStringSettings.TPJournalTitles; }
+        [XmlIgnore]
+        public ObservableCollection<GameHeadersFile> TPObjectiveTasks { get => currentStringSettings.TPObjectiveTasks; }
+        [XmlIgnore]
+        public ObservableCollection<GameHeadersFile> TPSpeechEventsJournals { get => currentStringSettings.TPSpeechEventsJournals; }
+        [XmlIgnore]
+        public ObservableCollection<GameHeadersFile> TPMapTextItems { get => currentStringSettings.TPMapTextItems; }
+        [XmlIgnore]
+        public ObservableCollection<GameHeadersFile> TPWorldNames { get => currentStringSettings.TPWorldNames; }
+        [XmlIgnore]
+        public ObservableCollection<GameHeadersFile> TPWorldDescriptions { get => currentStringSettings.TPWorldDescriptions; }
+
+        public ObservableCollection<TPGameStringsSettings> TPGameStringsSettings { get; } = new();
+
+        private TPGameStringsSettings currentStringSettings = new();
+
         public bool CreateBackupOnMapExport { get; set; } = true;
         [XmlIgnore]
         public string EffectsDirectory { get; set; } = string.Empty;
@@ -57,6 +74,22 @@ namespace TPMapEditor.Settings
         public string WorldFilesDirectory { get; set; } = string.Empty;
         [XmlIgnore]
         public string WorldObjectFilesDirectory { get; set; } = string.Empty;
+
+        partial void OnTpGamePathChanged(string value)
+        {
+            var tpGameFolderName = Path.GetFileNameWithoutExtension(value);
+            var newStringsSettings = TPGameStringsSettings.FirstOrDefault((o) => o.TPGameFolderName == tpGameFolderName);
+            if (newStringsSettings == null)
+            {
+                currentStringSettings = new() { TPGameFolderName = tpGameFolderName };
+                AddDefaultHeadersToLists();
+                TPGameStringsSettings.Add(currentStringSettings);
+            }
+            else
+            {
+                currentStringSettings = newStringsSettings;
+            }
+        }
 
         public void ReloadStringsDictionnaries(IProgress<string> progress, IProgress<string> logs)
         {
@@ -1092,49 +1125,12 @@ namespace TPMapEditor.Settings
 
         private void AddDefaultHeadersToLists()
         {
-            ClearHeadersLists();
-            TPTeamNames.Add(new("TPTEAMNAMES_GameStrings.h"));
-            TPSpeechEvents.Add(new("TPSPEECHEVENTS000_GameStrings.h"));
-            TPSpeechEvents.Add(new("TPSPEECHEVENTS001_GameStrings.h"));
-            TPSpeechEvents.Add(new("TPSPEECHEVENTS002_GameStrings.h"));
-            TPSpeechEvents.Add(new("TPSPEECHEVENTS003_GameStrings.h"));
-            TPSpeechEvents.Add(new("TPSPEECHEVENTS004_GameStrings.h"));
-            TPSpeechEvents.Add(new("TPSPEECHEVENTS005_GameStrings.h"));
-            TPSpeechEvents.Add(new("TPSPEECHEVENTTUTORIAL_GameStrings.h"));
-            TPSpeakerNames.Add(new("TPSPEAKERNAMES_GameStrings.h"));
-            TPShipNames.Add(new("TPCAMPAIGNSHIPNAMES00_GameStrings.h"));
-            TPShipNames.Add(new("TPCAMPAIGNSHIPNAMES01_GameStrings.h"));
-            TPShipNames.Add(new("TPCAMPAIGNSHIPNAMES02_GameStrings.h"));
-            TPShipNames.Add(new("TPSHIPNAMENAVY00_GameStrings.h"));
-            TPShipNames.Add(new("TPSHIPNAMENAVY01_GameStrings.h"));
-            TPShipNames.Add(new("TPSHIPNAMEPIRATE00_GameStrings.h"));
-            TPShipNames.Add(new("TPSHIPNAMEPIRATE01_GameStrings.h"));
-            TPShipNames.Add(new("TPSHIPNAMEPROCYON00_GameStrings.h"));
-            TPShipNames.Add(new("TPSHIPNAMEPROCYON01_GameStrings.h"));
-            TPInGameMessages.Add(new("TPINGAMEMESSAGE_GameStrings.h"));
-            TPJournalTitles.Add(new("TPJOURNALSCREEN_GameStrings.h"));
-            TPObjectiveTasks.Add(new("TPOBJECTIVES_GameStrings.h"));
-            TPObjectiveTasks.Add(new("TPOBJECTIVES2_GameStrings.h"));
-            TPSpeechEventsJournals.Add(new("TPSPEECHEVENTSJOURNALS_GameStrings.h"));
-            TPMapTextItems.Add(new("TPMAPTEXTITEMS_GameStrings.h"));
-            TPWorldNames.Add(new("TPWORLDNAMES_GameStrings.h"));
-            TPWorldNames.Add(new("TPJOURNALSCREEN_GameStrings.h"));
-            TPWorldDescriptions.Add(new("TPWORLDDESCRIPTION_GameStrings.h"));
+            currentStringSettings.AddDefaultHeadersToLists();
         }
 
         private void ClearHeadersLists()
         {
-            TPTeamNames.Clear();
-            TPSpeechEvents.Clear();
-            TPSpeakerNames.Clear();
-            TPShipNames.Clear();
-            TPInGameMessages.Clear();
-            TPJournalTitles.Clear();
-            TPObjectiveTasks.Clear();
-            TPSpeechEventsJournals.Clear();
-            TPMapTextItems.Clear();
-            TPWorldNames.Clear();
-            TPWorldDescriptions.Clear();
+            currentStringSettings.ClearHeadersLists();
         }
 
         public AppSettings Load()
@@ -1150,6 +1146,7 @@ namespace TPMapEditor.Settings
             catch
             {
                 AddDefaultHeadersToLists();
+                TPGameStringsSettings.Add(currentStringSettings);
                 Save();
                 return this;
             }
---
+++ TPMapEditor/Settings/TPGameStringsSettings.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPMapEditor.Data;
+
+namespace TPMapEditor.Settings
+{
+    /// <summary>
+    /// A class to allow per TPGame folder game headers usage
+    /// </summary>
+    public class TPGameStringsSettings
+    {
+        public string TPGameFolderName { get; set; } = "TP_Game";
+        public ObservableCollection<GameHeadersFile> TPTeamNames { get; } = new();
+        public ObservableCollection<GameHeadersFile> TPSpeechEvents { get; } = new();
+        public ObservableCollection<GameHeadersFile> TPSpeakerNames { get; } = new();
+        public ObservableCollection<GameHeadersFile> TPShipNames { get; } = new();
+        public ObservableCollection<GameHeadersFile> TPInGameMessages { get; } = new();
+        public ObservableCollection<GameHeadersFile> TPJournalTitles { get; } = new();
+        public ObservableCollection<GameHeadersFile> TPObjectiveTasks { get; } = new();
+        public ObservableCollection<GameHeadersFile> TPSpeechEventsJournals { get; } = new();
+        public ObservableCollection<GameHeadersFile> TPMapTextItems { get; } = new();
+        public ObservableCollection<GameHeadersFile> TPWorldNames { get; } = new();
+        public ObservableCollection<GameHeadersFile> TPWorldDescriptions { get; } = new();
+
+        public void AddDefaultHeadersToLists()
+        {
+            ClearHeadersLists();
+            TPTeamNames.Add(new("TPTEAMNAMES_GameStrings.h"));
+            TPSpeechEvents.Add(new("TPSPEECHEVENTS000_GameStrings.h"));
+            TPSpeechEvents.Add(new("TPSPEECHEVENTS001_GameStrings.h"));
+            TPSpeechEvents.Add(new("TPSPEECHEVENTS002_GameStrings.h"));
+            TPSpeechEvents.Add(new("TPSPEECHEVENTS003_GameStrings.h"));
+            TPSpeechEvents.Add(new("TPSPEECHEVENTS004_GameStrings.h"));
+            TPSpeechEvents.Add(new("TPSPEECHEVENTS005_GameStrings.h"));
+            TPSpeechEvents.Add(new("TPSPEECHEVENTTUTORIAL_GameStrings.h"));
+            TPSpeakerNames.Add(new("TPSPEAKERNAMES_GameStrings.h"));
+            TPShipNames.Add(new("TPCAMPAIGNSHIPNAMES00_GameStrings.h"));
+            TPShipNames.Add(new("TPCAMPAIGNSHIPNAMES01_GameStrings.h"));
+            TPShipNames.Add(new("TPCAMPAIGNSHIPNAMES02_GameStrings.h"));
+            TPShipNames.Add(new("TPSHIPNAMENAVY00_GameStrings.h"));
+            TPShipNames.Add(new("TPSHIPNAMENAVY01_GameStrings.h"));
+            TPShipNames.Add(new("TPSHIPNAMEPIRATE00_GameStrings.h"));
+            TPShipNames.Add(new("TPSHIPNAMEPIRATE01_GameStrings.h"));
+            TPShipNames.Add(new("TPSHIPNAMEPROCYON00_GameStrings.h"));
+            TPShipNames.Add(new("TPSHIPNAMEPROCYON01_GameStrings.h"));
+            TPInGameMessages.Add(new("TPINGAMEMESSAGE_GameStrings.h"));
+            TPJournalTitles.Add(new("TPJOURNALSCREEN_GameStrings.h"));
+            TPObjectiveTasks.Add(new("TPOBJECTIVES_GameStrings.h"));
+            TPObjectiveTasks.Add(new("TPOBJECTIVES2_GameStrings.h"));
+            TPSpeechEventsJournals.Add(new("TPSPEECHEVENTSJOURNALS_GameStrings.h"));
+            TPMapTextItems.Add(new("TPMAPTEXTITEMS_GameStrings.h"));
+            TPWorldNames.Add(new("TPWORLDNAMES_GameStrings.h"));
+            TPWorldNames.Add(new("TPJOURNALSCREEN_GameStrings.h"));
+            TPWorldDescriptions.Add(new("TPWORLDDESCRIPTION_GameStrings.h"));
+        }
+
+        public void ClearHeadersLists()
+        {
+            TPTeamNames.Clear();
+            TPSpeechEvents.Clear();
+            TPSpeakerNames.Clear();
+            TPShipNames.Clear();
+            TPInGameMessages.Clear();
+            TPJournalTitles.Clear();
+            TPObjectiveTasks.Clear();
+            TPSpeechEventsJournals.Clear();
+            TPMapTextItems.Clear();
+            TPWorldNames.Clear();
+            TPWorldDescriptions.Clear();
+        }
+    }
+}
